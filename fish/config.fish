f1cd98ed9098dabf874e58fe254d091abb40ef3a
rsbear <hellorosss@gmail.com>
2022-07-16 11:31:20 -0500

    ignorable

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set -U fish_greeting "🐟🐟🐟🐟"
+set -U fish_greeting ""
 
 # ALIASES
 alias fishy="nvim ~/.config/fish/config.fish"
@@ -21,8 +21,10 @@ alias ww="~/side/tmux-spaces/./tmux-spaces"
 alias llg="ls -l | rg ^d"
 alias cat="bat --theme=\$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo default || echo GitHub)"
 alias catp="cat package.json"
-alias ff="fz"
+alias ff="fzf"
 alias tko="tmux kill-server"
+
+alias edb="edgedb $argv"
 
 
 export EDITOR="nvim"
@@ -41,6 +43,14 @@ export PATH="/Users/rsbear/.deno/bin:$PATH"
 export ZK_NOTEBOOK_DIR="$HOME/.config/zk"
 
 export PATH="$HOME/.cargo/bin:$PATH"
+
+export PATH="/Users/rsbear/Library/Application Support/edgedb/bin:$PATH"
+
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS""
+ --color=fg:#908caa,bg:#191724,hl:#6e6a86
+ --color=fg+:#eb6f92,bg+:#191724,hl+:#908caa
+ --color=info:#9ccfd8,prompt:#f6c177,pointer:#c4a7e7
+ --color=marker:#ebbcba,spinner:#eb6f92,header:#ebbcba"
 
 
 starship init fish | source
